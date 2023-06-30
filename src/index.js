8409ec8f642e5042c41f07607fe7efe038ddbe18
Mario E Carrizosa Villarreal <mytcarrizosaland@gmail.com>
2023-06-30 08:04:02 -0600

    Mario8

--- src/index.js
+++ src/index.js
@@ -8,8 +8,8 @@ import store from './redux/store'
 import { Provider } from 'react-redux'
 import { PayPalScriptProvider } from '@paypal/react-paypal-js';
 
- //axios.defaults.baseURL = "http://localhost:3001/";
-axios.defaults.baseURL = "https://pf-back-gamezone-production-f9d0.up.railway.app/"
+axios.defaults.baseURL = "http://localhost:3000/";
+//axios.defaults.baseURL = "https://pf-back-gamezone-production-f9d0.up.railway.app/"
 //axios.defaults.baseURL = "https://back-gamezone01.onrender.com/"
 
 ReactDOM.render(
