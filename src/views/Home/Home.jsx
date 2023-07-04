29b5886337a021c1986a385e65c2a285e98b24cf
fernandotejada1997 <fernandotejada1997@gmail.com>
2023-07-04 17:08:33 -0300

    arreglo de loading

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -5,30 +5,35 @@ import { useDispatch, useSelector } from "react-redux";
 import style from "./Home.module.css";
 import * as act from "../../redux/actions";
 import { faSearchengin } from "@fortawesome/free-brands-svg-icons";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useHistory } from "react-router-dom";
+import { PacmanLoader } from "react-spinners";
 
 const Home = () => {
   const history = useHistory();
   const dispatch = useDispatch();
-  const gameOffer = useSelector(state => state.gameOffer);
-  const search = useSelector(state => state.search);
-  const gamesNewReleases = useSelector(state => state.gamesNewReleases);
-  const gamesTopSellers = useSelector(state => state.gamesTopSellers);
-  const games = useSelector(state => state.games);
+  const gameOffer = useSelector((state) => state.gameOffer);
+  const search = useSelector((state) => state.search);
+  const gamesNewReleases = useSelector((state) => state.gamesNewReleases);
+  const gamesTopSellers = useSelector((state) => state.gamesTopSellers);
+  const games = useSelector((state) => state.games);
   const [name, setName] = useState("");
-  //console.log(games);
-  
-  useEffect(() => {
-    dispatch(act.getGames());
-    // dispatch(act.getGamesOffer());
-    // dispatch(act.getGamesNewReleases());
-    // dispatch(act.getGamesComingSoon());
-    // dispatch(act.getGamesTopSellers());
-    dispatch(act.clearSearch());
-    // dispatch(act.preload());
-  }, [dispatch]);
+  const [loading, setLoading] = useState(true); // Estado para controlar la carga
 
+  useEffect(() => {
+    const loadData = async () => {
+      await dispatch(act.getGames());
+      // await dispatch(act.getGamesOffer());
+      // await dispatch(act.getGamesNewReleases());
+      // await dispatch(act.getGamesComingSoon());
+      // await dispatch(act.getGamesTopSellers());
+      await dispatch(act.clearSearch());
+      // await dispatch(act.preload());
+      setLoading(false); // Marcar como cargado cuando termina la carga de datos
+    };
+
+    loadData();
+  }, [dispatch]);
 
   const handleChange = (e) => {
     e.preventDefault();
@@ -50,28 +55,37 @@ const Home = () => {
 
   // Verificar si el array de juegos tiene al menos 14 elementos
   const selectedGames = games.length >= 18 ? games.slice(0, 18) : games;
-  const frees = games.filter(game => game.is_free === true && game.price_overview >= 0)
-  const type =   games.filter(game => game.type === "demo" || game.type === "dlc");
-  const controllerSupport =  games.filter(game => game.controller_support === "full");
+  const frees = games.filter(
+    (game) => game.is_free === true && game.price_overview >= 0
+  );
+  const type = games.filter(
+    (game) => game.type === "demo" || game.type === "dlc"
+  );
+  const controllerSupport = games.filter(
+    (game) => game.controller_support === "full"
+  );
 
-  const frees2 =  frees.slice(0, 12);
+  const frees2 = frees.slice(0, 12);
   const type2 = type.slice(0, 12);
-  const controllerSupport2 = controllerSupport.slice(0, 12) ;
+  const controllerSupport2 = controllerSupport.slice(0, 12);
 
+            if (loading) {
+              return (
+              <div className={style.loading}>
+                <PacmanLoader color="blue" size={80} speedMultiplier={1} />
+              </div>// Muestra el mensaje de carga mientras loading es true
+            )}
 
-
-  console.log(selectedGames)
   return (
     <div className={style.homeContainer}>
       <Carousel />
       <div>
-
         <div className={style.searchcontainer}>
           <input
             className={style.search}
             placeholder="search..."
             type="text"
-            onChange={e => handleChange(e)}
+            onChange={(e) => handleChange(e)}
             onKeyDown={handleKeyDown}
           />
           <FontAwesomeIcon
@@ -86,11 +100,11 @@ const Home = () => {
         <CardsContainer gameComingSoon={selectedGames} />
       </div>
       <h3 className={style.title}>Free</h3>
-        <CardsContainer gameComingSoon={frees2} />
-        <h3 className={style.title}>Demos and DLCs</h3>
-        <CardsContainer gameComingSoon={type2} />
-        <h3 className={style.title}>Controllers</h3>
-        <CardsContainer gameComingSoon={controllerSupport2} />
+      <CardsContainer gameComingSoon={frees2} />
+      <h3 className={style.title}>Demos and DLCs</h3>
+      <CardsContainer gameComingSoon={type2} />
+      <h3 className={style.title}>Controllers</h3>
+      <CardsContainer gameComingSoon={controllerSupport2} />
       {/* <h3 className={style.title}>Top Sells</h3>
       <CardsContainer gameComingSoon={gamesTopSellers} />
       <h3 className={style.title}>Game Offers</h3>
